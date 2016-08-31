ff72cd6686f28665f4b3cf568d09342b83fc2d03
Vallerboy <10135886>
2016-08-31 18:36:43 +0200

    Update v2

--- app/src/main/java/pl/akademiakodu/kurs2/MainActivity.java
+++ app/src/main/java/pl/akademiakodu/kurs2/MainActivity.java
@@ -1,13 +1,18 @@
 package pl.akademiakodu.kurs2;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends Activity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
 }
