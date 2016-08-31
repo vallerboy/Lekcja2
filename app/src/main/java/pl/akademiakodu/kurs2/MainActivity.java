5cdf75f41750f80d938a14637bfe2a9244be2f15
Vallerboy <10135886>
2016-08-31 18:43:10 +0200

    Update v3

--- app/src/main/java/pl/akademiakodu/kurs2/MainActivity.java
+++ app/src/main/java/pl/akademiakodu/kurs2/MainActivity.java
@@ -2,16 +2,24 @@ package pl.akademiakodu.kurs2;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.BindView;
 
 
 public class MainActivity extends Activity {
 
+    @BindView(R.id.textView2)
+    private TextView text;
+
+    //TODO cos tam
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+       // text = (TextView) findViewById(R.id.textView2);
 
     }
 
