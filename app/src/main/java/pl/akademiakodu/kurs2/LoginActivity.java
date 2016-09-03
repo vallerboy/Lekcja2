f38815810a2283d713ad42678865af520d0bddf5
Vallerboy <10135886>
2016-09-03 11:14:36 +0200

    test

---
+++ app/src/main/java/pl/akademiakodu/kurs2/LoginActivity.java
@@ -0,0 +1,30 @@
+package pl.akademiakodu.kurs2;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class LoginActivity extends Activity {
+
+    @BindView(R.id.responseText)
+    public TextView textView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+        Intent i = getIntent();
+        if (i.hasExtra("key")) {
+            String response = i.getStringExtra("key");
+            textView.setText(response);
+        }
+
+    }
+}
